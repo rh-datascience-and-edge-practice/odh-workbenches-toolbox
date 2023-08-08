2ab5c87679dbfbc308ca8a853847a7479614a8b6
David Williams <dww@pacificera.com>
2023-08-08 05:42:26 -0600

    updated to pull from quay

--- odh-workbench.sh
+++ odh-workbench.sh
@@ -6,5 +6,4 @@ podman run \
     --security-opt label=type:nvidia_container_t \
     -v $(pwd)/Notebook:/opt/app-root/src:Z \
     -p 8888:8888 \
-       localhost:5000/opendatahub/workbench-images:jupyter-pytorch-ubi9-python-3.9-2023a_20230807
-       
+       quay.io/opendatahub/workbench-images:cuda-jupyter-minimal-ubi9-python-3.9-2023a-20230807
